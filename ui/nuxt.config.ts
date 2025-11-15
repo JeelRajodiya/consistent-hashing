1e72e44eb82a196e039aa667de828ca586e433f4
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2025-11-15 23:30:04 +0530

    setup nuxt ui

--- ui/nuxt.config.ts
+++ ui/nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
-})
+  modules: ["@nuxt/ui", "@nuxtjs/color-mode"],
+  css: ["~/assets/css/main.css"],
+
+  colorMode: {
+    preference: "light",
+    fallback: "light",
+  },
+});
